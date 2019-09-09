ca10086c1f861b407bd5767f12fc0fc27c7b9eef
Levi Miller <paullevimiller@gmail.com>
2019-09-08 19:00:25 -0700

    Modify carrier_wave initializer to fix AWS constant error

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,15 +1,19 @@
-if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        # Configuration for Amazon S3
-        :provider              => 'AWS',
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-        :aws_signature_version => 4,
-        :region                => ENV['S3_REGION']
-      }
-      config.fog_directory     =  ENV['S3_BUCKET']
-      config.fog_provider = 'fog/aws'
-      config.fog_attributes = { :s3_signature_version => :v4 }
-    end
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+if Rails.env.production? || Rails.env.staging?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws '
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION'],
+      :aws_signature_version => 4
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_attributes    = { :s3_signature_version => :v4 }
   end
+end
