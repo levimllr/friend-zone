a91e5333bc29a2b27cd465703cef57c3044ca0e1
Levi Miller <paullevimiller@gmail.com>
2019-09-08 17:58:19 -0700

    Added config.fog_attributes to address unrecognized arguments

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,5 +9,6 @@ if Rails.env.production?
       }
       config.fog_directory     =  ENV['S3_BUCKET']
       config.fog_provider = 'fog/aws'
+      config.fog_attributes = { :s3_signature_version => :v4 }
     end
   end
