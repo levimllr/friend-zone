2abcb7c0dbd73fe8e07429f81a53e04c3face938
Levi Miller <paullevimiller@gmail.com>
2019-10-23 15:58:18 -0700

    Added meeting CRUD

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,16 +1,17 @@
 class MeetingsController < ApplicationController
   before_action :logged_in_person, only: [:create, :destroy]
+  before_action :find_meeting_by_id, only: [:show, :edit, :update, :destroy]
 
   def create
     byebug
-    @meeting = Meeting.new
+    @meeting = Meeting.new(meeting_params)
     if @meeting.save
       flash[:success] = 'Meeting created!'
     else
       flash[:danger] = 'Meeting not created. Missing friend, place, or type!'
     end
-    current_person.meetings << @meeting
-    People.find(meeting_params[:people]) << @meeting
+    # current_person.meetings << @meeting
+    # People.find(meeting_params[:people]) << @meeting
     redirect_to(root_url)
   end
 
@@ -25,15 +26,30 @@ class MeetingsController < ApplicationController
   def new
   end
 
+  def edit
+    @new_meeting = find_meeting_by_id
+
+  end
+
+  def update
+    # byebug
+    @meeting.update(meeting_params)
+    # current_person.meetings << @meeting
+    redirect_to(meetings_path)
+  end
+
   def destroy
+    @meeting.destroy
+    redirect_to(meetings_path)
   end
 
   # Only allow note content
   def meeting_params
     # byebug
-    params.require(:meeting).permit(
-      :meeting_type, :when, :location,
-      person_attributes: [:id]
-    )
+    params.require(:meeting).permit(:meeting_type, :when, :location, person_ids: [])
+  end
+
+  def find_meeting_by_id
+    @meeting = Meeting.find(params[:id])
   end
 end
